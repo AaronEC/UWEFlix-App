0f1ade412d6a6ec0043e0a083adee7e08d6589a8
AaronEC <aaron_cardwell@hotmail.com>
2022-05-08 19:46:27 +0100

    Movies lists added (check mark)

--- uweflixapp/urls.py
+++ uweflixapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from .views import Home, ProfileList, ProfileCreate
+from .views import Home, MovieList, ProfileList, ProfileCreate, MovieDetail
 
 app_name = 'uweflixapp'
 
 urlpatterns = [
     path('', Home.as_view(), name="Home"),
     path('profiles/', ProfileList.as_view(), name="profile-list"),
-    path('profiles/create', ProfileCreate.as_view(), name="profile-create")
+    path('profiles/create', ProfileCreate.as_view(), name="profile-create"),
+    path('watch/<str:profile_id>', MovieList.as_view(), name="movie-list"),
+    path('watch/detail/<str:movie_id>', MovieDetail.as_view(), name="movie-detail")
     ]
